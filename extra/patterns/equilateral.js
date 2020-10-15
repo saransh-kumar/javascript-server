da3c504b9c23adda3db0c561c31ce6638b9f21da
Saransh Kumar <72736644+saransh-kumar@users.noreply.github.com>
2020-10-15 17:03:08 +0530

    docs: add diamond.js equilateral.js file

---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,26 @@
+
+function equilateral(rows){
+    if(rows<2 || rows>10){
+        console.log("Input range between 2-10");
+    }
+    else{
+        let size = rows
+        for(let i=0;i<rows;i++){
+            for(let j=0;j<rows;j++){
+                if(j == size-1){
+                    process.stdout.write("* ");
+                    size -= 1;
+                }
+                else if(j<size){
+                    process.stdout.write(" ");
+                }
+                else{
+                    process.stdout.write("* ");
+                }
+            }
+            console.log();
+        }
+    }
+}
+let rows = 6;
+equilateral(process.argv[2]);
